07cbe287be6a94b8b5f7292b2b46f86f1c5289e6
jinzhongjia <mail@nvimer.org>
2024-03-17 13:14:40 +0800

    basic call support

--- examples/main.zig
+++ examples/main.zig
@@ -1,7 +1,6 @@
 //! This file is not an example, it is a file that is retained for testing during the development of this package.
 const std = @import("std");
 const znvim = @import("znvim");
-const rpc = @import("../src/rpc.zig");
 
 const address = "127.0.0.1";
 const port = 9090;
@@ -18,14 +17,15 @@ pub fn main() !void {
     const stream = try std.net.tcpConnectToAddress(try std.net.Address.parseIp4(address, port));
     defer stream.close();
 
-    const RpcType = rpc.rpcClientType(20480, .socket);
-    var r = try RpcType.init(stream, stream, allocator);
+    const ClientType = znvim.Client(20480, .socket);
+    var r = try ClientType.init(stream, stream, allocator);
     defer r.deinit();
 
-    const arr = try rpc.Payload.arrPayload(0, allocator);
-    defer arr.free(allocator);
+    const params = try ClientType.createParams(0, allocator);
+    defer ClientType.freeParams(params, allocator);
 
-    const val = try r.call("nvim_get_current_buf", arr);
-    std.debug.print("{any}\n", .{val.result});
-    r.freeResultType(val);
+    const result = try r.call("nvim_get_current_buf", params);
+    defer r.freeResultType(result);
+
+    std.log.info("channel id is {}", .{result.result});
 }
