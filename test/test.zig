e85b77c771780ce3e82b94da50ef6392a4cece90
jinzhongjia <mail@nvimer.org>
2024-03-18 20:11:22 +0800

    update uint-test

--- test/test.zig
+++ test/test.zig
@@ -7,35 +7,17 @@ const expect = std.testing.expect;
 
 const args = [_][]const u8{ "nvim", "--embed" };
 
-const ClientFileType = znvim.DefaultClientType(struct {}, .file);
+const ClientFileType = znvim.DefaultClient(.file);
 
 test "basic embed connect" {
     var nvim = try create_nvim_process();
 
-    const client = try ClientFileType.init(
+    var client = try ClientFileType.init(
         nvim.stdin.?,
         nvim.stdout.?,
         allocator,
-        true,
     );
     defer client.deinit();
-    _ = try nvim.kill();
-}
-
-test "call with reader" {
-    var nvim = try create_nvim_process();
-
-    const client = try ClientFileType.init(
-        nvim.stdin.?,
-        nvim.stdout.?,
-        allocator,
-        true,
-    );
-    defer client.deinit();
-
-    const reader = try client.call_with_reader(.nvim_get_current_buf, .{}, allocator);
-    const res = try reader.read_ext(allocator);
-    defer allocator.free(res.data);
 
     _ = try nvim.kill();
 }
@@ -43,57 +25,18 @@ test "call with reader" {
 test "call" {
     var nvim = try create_nvim_process();
 
-    const client = try ClientFileType.init(
+    var client = try ClientFileType.init(
         nvim.stdin.?,
         nvim.stdout.?,
         allocator,
-        true,
     );
     defer client.deinit();
 
-    const buffer = try client.call(.nvim_get_current_buf, .{}, allocator);
-    defer allocator.free(buffer.data);
+    const params = try ClientFileType.createParams(0, allocator);
+    defer ClientFileType.freeParams(params, allocator);
 
-    _ = try nvim.kill();
-}
-
-test "call with writer (get reader with writer)" {
-    var nvim = try create_nvim_process();
-
-    const client = try ClientFileType.init(
-        nvim.stdin.?,
-        nvim.stdout.?,
-        allocator,
-        true,
-    );
-    defer client.deinit();
-
-    const writer = try client.call_with_writer(.nvim_get_current_buf);
-    try writer.write_array_header(0);
-
-    const reader = try client.get_reader_with_writer(writer, allocator);
-    const res = try reader.read_ext(allocator);
-    allocator.free(res.data);
-
-    _ = try nvim.kill();
-}
-
-test "call with writer (get result with writer)" {
-    var nvim = try create_nvim_process();
-
-    const client = try ClientFileType.init(
-        nvim.stdin.?,
-        nvim.stdout.?,
-        allocator,
-        true,
-    );
-    defer client.deinit();
-
-    const writer = try client.call_with_writer(.nvim_get_current_buf);
-    try writer.write_array_header(0);
-
-    const buffer = try client.get_result_with_writer(znvim.Buffer, writer, allocator);
-    defer allocator.free(buffer.data);
+    const res = try client.call("nvim_get_current_buf", params);
+    defer client.freeResultType(res);
 
     _ = try nvim.kill();
 }
