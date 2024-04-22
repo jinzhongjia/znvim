0ab73a3adc09b71eb60b460a5d7de4f406f43bdd
jinzhongjia <mail@nvimer.org>
2024-04-22 17:36:26 +0800

    Update

---
+++ build_13.zig
@@ -0,0 +1,66 @@
+const std = @import("std");
+const Build = std.Build;
+const Module = Build.Module;
+const ResolvedTarget = Build.ResolvedTarget;
+const OptimizeMode = std.builtin.OptimizeMode;
+
+pub fn build(b: *Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // get msgpack
+    const msgpack = b.dependency("zig-msgpack", .{});
+
+    // create module
+    const znvim = b.addModule("znvim", .{
+        .root_source_file = .{
+            .path = "src/znvim.zig",
+        },
+        .imports = &.{
+            .{
+                .name = "msgpack",
+                .module = msgpack.module("msgpack"),
+            },
+        },
+    });
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "test/test.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    unit_tests.root_module.addImport("znvim", znvim);
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    run_unit_tests.skip_foreign_checks = true;
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+
+    // create exe
+    create_exe(b, target, optimize, znvim);
+}
+
+fn create_exe(b: *Build, target: ResolvedTarget, optimize: OptimizeMode, znvim: *Module) void {
+    const exe = b.addExecutable(.{
+        .name = "znvim",
+        .root_source_file = .{ .path = "examples/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.root_module.addImport("znvim", znvim);
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
